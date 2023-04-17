0430d596b46e131a1647218eeb80aaaed62670fc
Nishant Bhagat <64642374+Nishantbhagat57@users.noreply.github.com>
2023-04-17 19:19:05 +0530

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,6 +1,4 @@
 #! /bin/bash
-printf "Installing op-server... \nThis might take a while... " >&2
-{
 sudo useradd -m op
 sudo adduser op sudo
 echo 'op:##Teki@123' | sudo chpasswd
@@ -20,16 +18,4 @@ sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
 sudo adduser op chrome-remote-desktop
-} &> /dev/null &&
-printf "\nSetup Complete \nCreated new user op with password: ##Teki@123 \nYou can change both op's password with passwd(with no sudo) \nOr you can change root's password with sudo passwd" >&2 ||
-printf "\nError Occured " >&2
-printf '\nCheck https://remotedesktop.google.com/headless and copy command of Debian Linux \n'
-read -p "Paste Here: " CRP
-su - op -c """$CRP"""
-printf 'Check https://remotedesktop.google.com/access/ \n\n'
-if sudo apt-get upgrade &> /dev/null
-then
-    printf "\n\nUpgrade Completed " >&2
-else
-    printf "\n\nError Occured " >&2
-fi
+sudo apt-get upgrade
