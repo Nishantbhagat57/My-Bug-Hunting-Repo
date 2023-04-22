bbabafdcd70d5d12fe4521e0a0f95f39fddfa17d
Nishant Bhagat <64642374+Nishantbhagat57@users.noreply.github.com>
2023-04-22 21:25:02 +0530

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,17 +1,12 @@
 #! /bin/bash
 sudo apt-get install ubuntu-desktop 
-sudo apt-get update
+sudo add-apt-repository -r ppa:yorba/ppa
+sudo apt-get update && sudo apt-get upgrade
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
-sudo dpkg --install chrome-remote-desktop_current_amd64.deb
-sudo apt install --assume-yes --fix-broken
-sudo DEBIAN_FRONTEND=noninteractive \
-apt install --assume-yes xfce4 desktop-base
-sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
-sudo apt install --assume-yes xscreensaver
-sudo systemctl disable lightdm.service
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg --install google-chrome-stable_current_amd64.deb
-sudo apt install --assume-yes --fix-broken
+apt install chrome-remote-desktop_current_amd64.deb
+sudo DEBIAN_FRONTEND=noninteractive apt install --assume-yes xfce4 desktop-base dbus-x11 xscreensaver
+sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'
+sudo apt install xfce4  xfce4-goodies -y
 sudo apt install nautilus nano -y 
 sudo adduser teki chrome-remote-desktop
 sudo apt-get upgrade
