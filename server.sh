5f26a355272589b943aa5f43056c5964fa751517
Nishant Bhagat <64642374+Nishantbhagat57@users.noreply.github.com>
2023-04-22 21:48:05 +0530

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,12 +1,9 @@
 #! /bin/bash
-sudo apt-get install ubuntu-desktop 
-sudo add-apt-repository -r ppa:yorba/ppa
-sudo apt-get update && sudo apt-get upgrade
+sudo apt update
+sudo apt install --assume-yes wget tasksel
+apt-get install ubuntu-desktop
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
-apt install chrome-remote-desktop_current_amd64.deb
-sudo DEBIAN_FRONTEND=noninteractive apt install --assume-yes xfce4 desktop-base dbus-x11 xscreensaver
-sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'
-sudo apt install xfce4  xfce4-goodies -y
-sudo apt install nautilus nano -y 
-sudo adduser teki chrome-remote-desktop
-sudo apt-get upgrade
+sudo apt-get install --assume-yes ./chrome-remote-desktop_current_amd64.deb
+sudo DEBIAN_FRONTEND=noninteractive \
+    apt install --assume-yes  task-gnome-desktop
+sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/gnome-session" > /etc/chrome-remote-desktop-session'
