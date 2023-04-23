5ac7fcc8a57d785492c2b5c0d2ed2b5d9b4d2819
Nishant Bhagat <64642374+Nishantbhagat57@users.noreply.github.com>
2023-04-23 19:55:47 +0530

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 sudo apt update
-apt-get install ubuntu-desktop
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo apt-get install --assume-yes ./chrome-remote-desktop_current_amd64.deb
 sudo apt install xfce4  xfce4-goodies -y
