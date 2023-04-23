90c31a42d165f4c89cf316a23ada2d1ed9c5817b
Nishant Bhagat <64642374+Nishantbhagat57@users.noreply.github.com>
2023-04-23 19:38:14 +0530

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,9 +1,7 @@
 #! /bin/bash
 sudo apt update
-sudo apt install --assume-yes wget tasksel
 apt-get install ubuntu-desktop
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo apt-get install --assume-yes ./chrome-remote-desktop_current_amd64.deb
-sudo DEBIAN_FRONTEND=noninteractive \
-    apt install --assume-yes  task-gnome-desktop
-sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/gnome-session" > /etc/chrome-remote-desktop-session'
+sudo apt install xfce4  xfce4-goodies -y
+sudo apt update && sudo apt upgrade 
