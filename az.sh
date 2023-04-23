4c79bbf4b785320bccd010bd2139cd7183e83021
Nishant Bhagat <64642374+Nishantbhagat57@users.noreply.github.com>
2023-04-23 20:16:13 +0530

    Update az.sh

--- az.sh
+++ az.sh
@@ -1,1 +1,1 @@
-DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AVHEtk4VqECOb4PJPH5pIAoANhZruxMdJ2Pqo0mPCGDch6jsaCUStuL3K7DGP0YLlMARiw" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname)
+DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AVHEtk4IAx3u0Y24jnkgtahyRzm1yGcsg_Gm-b2LNSuR0yz1bxunlyBnylYKSkUrfP1lJA" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname)
